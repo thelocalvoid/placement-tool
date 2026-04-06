4f9ee9a5c3785774b8a6a0536af8577a9894b4e1
voids-place <clrkdigital@gmail.com>
2026-04-06 19:05:16 +1000

    v1.0.1 Added command register for loop

--- commands.lua
+++ commands.lua
@@ -4,23 +4,38 @@
     
 -- end, true)
 
-RegisterCommand("ppt_opentool", function (src)
-    TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_opentool")
+-- RegisterCommand("ppt_opentool", function (src)
+--     TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_opentool")
     
-end, true)
-RegisterCommand("ppt_closetool", function (src)
-    TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_closetool")
+-- end, true)
+-- RegisterCommand("ppt_closetool", function (src)
+--     TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_closetool")
     
-end, true)
-RegisterCommand("ppt_2dmap", function (src)
-    TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_2dmap")
+-- end, true)
+-- RegisterCommand("ppt_2dmap", function (src)
+--     TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_2dmap")
     
-end, true)
-RegisterCommand("ppt_3dmap", function (src)
-    TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_3dmap")
+-- end, true)
+-- RegisterCommand("ppt_3dmap", function (src)
+--     TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_3dmap")
     
-end, true)
-RegisterCommand("ppt_3dfreecam", function (src)
-    TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_3dfreecam")
+-- end, true)
+-- RegisterCommand("ppt_3dfreecam", function (src)
+--     TriggerClientEvent("postplace-goiufgSDGFI:com", src, "ppt_3dfreecam")
     
-end, true)
+-- end, true)
+
+local Commands = {
+    "ppt_opentool",
+    "ppt_closetool",
+    "ppt_2dmap",
+    "ppt_3dmap",
+    "ppt_3dfreecam",
+}
+
+for key, value in pairs(Commands) do
+    RegisterCommand(value, function (src)
+        TriggerClientEvent("postplace-goiufgSDGFI:com", src, value)
+        
+    end, true)
+end
